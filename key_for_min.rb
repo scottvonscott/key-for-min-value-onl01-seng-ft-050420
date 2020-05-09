328c62d04b6b61652e22e698477d3277e474aad4
Scott  Keplinger <skeplin17@gmail.com>
2020-05-09 23:46:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(hash)
   array << value
  end
  hash.collect do |key, value|
-  if array.include? (array < value)
+  if array.include? (value > array[0])
      next
    else
     key
