00ec32781aab16954a12814451b5a8cc62824dce
Scott  Keplinger <skeplin17@gmail.com>
2020-05-10 01:11:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,14 @@
 def key_for_min_value(hash)
   array = []
+  min_value = nil
+  min_key = nil
  hash.each do |key, value|
   array << value
+  if
+    min_value == nil || value < min_value
+     min_value << value
+     min_key << key
+  end
  end
- hash.each do |key, value|
-    if value > array.any?
-      next
-    else
-      return key
-   end
- end
+ return min_key
 end
